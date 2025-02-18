ca5613f76269545e3d5332529f14c24a9459b326
Siba-Ka-Playground <bariksiba33@gmail.com>
2025-02-19 02:35:56 +0530

    Content Updated (Side Bar)

--- dashboard.js
+++ dashboard.js
@@ -1,1 +1,6 @@
-
+function toggleSidebar() {
+  const sidebar = document.getElementById("sidebar");
+  const content = document.getElementById("content");
+  sidebar.classList.toggle("open");
+  content.classList.toggle("shifted");
+}
