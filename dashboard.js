81b013932757cbde6ef12621b6fa8cb8304acd41
Siba-Ka-Playground <bariksiba33@gmail.com>
2025-02-19 18:25:59 +0530

    Updated

--- dashboard.js
+++ dashboard.js
@@ -1,6 +1,10 @@
 function toggleSidebar() {
   const sidebar = document.getElementById("sidebar");
   const content = document.getElementById("content");
+  const h_detail = document.getElementsById("h_detail");
+  const b_detail = document.getElementsById("b_detail");
   sidebar.classList.toggle("open");
   content.classList.toggle("shifted");
+  h_detail.classList.toggle("detail");
+  b_detail.classList.toggle("detail");
 }
