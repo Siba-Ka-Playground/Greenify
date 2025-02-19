13e45b6a1a03fa7a0c136cb37a1f15ff523542b0
Siba-Ka-Playground <bariksiba33@gmail.com>
2025-02-19 19:00:35 +0530

    SideBar Updated

--- dashboard.js
+++ dashboard.js
@@ -1,10 +1,15 @@
+// Side Bar Toogle works
 function toggleSidebar() {
   const sidebar = document.getElementById("sidebar");
   const content = document.getElementById("content");
-  const h_detail = document.getElementsById("h_detail");
-  const b_detail = document.getElementsById("b_detail");
+  const h_detail = document.getElementById("h_detail");
+  const b_detail = document.getElementById("b_detail");
+  const r_detail = document.getElementById("r_detail");
+  const c_detail = document.getElementById("c_detail");
   sidebar.classList.toggle("open");
   content.classList.toggle("shifted");
   h_detail.classList.toggle("detail");
   b_detail.classList.toggle("detail");
+  r_detail.classList.toggle("detail");
+  c_detail.classList.toggle("detail");
 }
